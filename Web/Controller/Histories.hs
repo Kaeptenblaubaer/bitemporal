99b252cf43d0a72548d8f870700879b83958ba90
michael.fliegner <michael.fliegner@hamburg.de>
2021-04-10 19:32:10 +0200

    fst buggy version of history tree show

--- Web/Controller/Histories.hs
+++ Web/Controller/Histories.hs
@@ -17,14 +17,8 @@ instance Controller HistoriesController where
         render NewView { .. }
 
     action ShowHistoryAction { historyId } = do
-        history :: history <- fetch historyId >>= fetchRelated #versions 
+        history :: History <- fetch historyId
         versions :: [Version]<- sqlQuery "SELECT * FROM versions WHERE ref_history = ? order by createdat desc" (Only historyId)
-        let (roots,descendants) :: ([Version],[Version]) = partition (\v -> isNothing $ get #refShadowedby v) versions
-        forEach versions (\v -> putStrLn $ ">>>>> version: " ++ show v) 
-        forEach roots (\v -> putStrLn $ ">>>>> valid: " ++ show v) 
-        forEach descendants (\v -> putStrLn $ ">>>>> invalid: " ++ show v) 
-        let forest = mkForest versions []
-        putStrLn $ ">>>>>>>>>>TREE\n " ++ show forest
         render ShowView { .. }
 
     action EditHistoryAction { historyId } = do
@@ -41,7 +35,7 @@ instance Controller HistoriesController where
                     history <- history |> updateRecord
                     setSuccessMessage "History updated"
                     redirectTo EditHistoryAction { .. }
-
+ 
     action CreateHistoryAction = do
         let history = newRecord @History
         history
@@ -62,24 +56,3 @@ instance Controller HistoriesController where
 buildHistory history = history
     |> fill @["latestversion","historyType"]
 
-compId2refId :: Version -> Version -> Bool
-compId2refId v ref = (Just $ get #id v) == get #refShadowedby ref
-
-isChild node parent = compId2refId node parent  
-
-mkTree root [] = (Node root [],[]) 
-mkTree root rest = let  (children,rest2) = partition (isChild root) rest
-                        (subforest,rest3) = mkSubForest children [] rest2 
-    in (Node root subforest,rest3)
-
-mkSubForest :: [Version] -> [Tree Version] -> [Version] -> ( [Tree Version], [Version])
-mkSubForest [] accu rest = (accu, rest)
-mkSubForest (fstChild:siblings) accu rest  = 
-        let (tree,newrest) = mkTree fstChild rest 
-        in mkSubForest siblings (accu++[tree]) newrest
-
-mkForest ::  [Version] -> [Tree Version] -> ( [Tree Version], [Version])
-mkForest  [] akku               = (akku,[])
-mkForest  (head:tail) akku      =
-        let (tree,rest) = mkTree head tail 
-        in mkForest rest (akku ++ [tree]) 
--- Web/View/Histories/Show.hs
+++ Web/View/Histories/Show.hs
@@ -1,7 +1,7 @@
 module Web.View.Histories.Show where
 import Web.View.Prelude
-
-data ShowView = ShowView { history :: Include "versions" History }
+import Data.Tree
+data ShowView = ShowView { history :: History , versions :: [Version] }
 
 instance View ShowView where
     html ShowView { .. } = [hsx|
@@ -12,37 +12,64 @@ instance View ShowView where
             </ol>
         </nav>
         <h1>Show History</h1>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>HistoryType</th>
-                    </tr>
-                    </thead>
-                    <tr>
-                        <td>{get #id history}</td><td>{get #historyType history}</td>
-                    </tr>   
-                <tbody>
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th><h2>Versions</h2></th>
-                        </tr>
-                            <tr>
-                                <th>ID</th>
-                                <th>validfrom</th>
-                                <th>Command</th>
-                            </tr>
-                        </thead>
-                        <tbody><div>{forEach (get #versions history) (renderVersion history)}</div></tbody>
-                    </table>
-                </tbody>
-            </table>
-        
+        <h2 > Tree representation of bitemporal versioning histories</h2>
+        <p>reference time is displayed vertically running down from old to new. Indentation represents
+        invalidation of timelines by retrospective mutatations </p>
+        <p>Click on the arrow(s) to open or close the tree branches, that is: show or hide invalidated timelines</p>
+        <ol id="history" >
+          { forEach (fst $ mkForest versions []) renderTree } 
+        </ol>
     |]
 
-renderVersion history version = [hsx|
-    <tr>
-        <td>{get #id version}</td><td>{get #validfrom version}</td><td><a href={pathTo(NewWorkflowAction) <> "?historyId=" ++ (show (get #id history))}>Start Mutation Workflow</a></td>
-    </tr>
-|]
+renderTree :: Tree Version -> Html
+renderTree n  = case subForest n of
+       [] ->  [hsx| 
+                    <li>
+                            {renderLabel n}
+                    </li>
+                |]
+       _  ->  [hsx|  
+                    <li>{renderLabel n} 
+                        <span class="caret" ></span>
+                        <ol class="nested">
+                          { forEach (subForest n) renderTree } 
+                        </ol>
+                    </li>
+                |]
+
+
+renderLabel :: Tree Version -> Html
+renderLabel n = render (rootLabel n)
+    where render version = [hsx| <div><table><tr>
+            <td>{get #id version}</td><td>{get #validfrom version}</td><td><a href={pathTo(NewWorkflowAction) <> "?historyId=" ++ (show (get #refHistory version))}>Start Mutation Workflow</a></td>
+            </tr></table></div>
+          |] 
+
+-- <a href={pathTo(NewWorkflowAction) <> "?historyId=" ++ (show (get #id refHistory))}>Start Mutation Workflow</a>
+
+
+bubu history = do
+    let versionForest = mkForest (get #versions history) []
+    putStrLn $ ">>>>>>>>>>TREE\n " ++ show versionForest
+
+compId2refId :: Version -> Version -> Bool
+compId2refId v ref = (Just $ get #id v) == get #refShadowedby ref
+
+isChild node parent = compId2refId node parent  
+
+mkTree root [] = (Node root [],[]) 
+mkTree root rest = let  (children,rest2) = partition (isChild root) rest
+                        (subforest,rest3) = mkSubForest children [] rest2 
+    in (Node root subforest,rest3)
+
+mkSubForest :: [Version] -> [Tree Version] -> [Version] -> ( [Tree Version], [Version])
+mkSubForest [] accu rest = (accu, rest)
+mkSubForest (fstChild:siblings) accu rest  = 
+        let (tree,newrest) = mkTree fstChild rest 
+        in mkSubForest siblings (accu++[tree]) newrest
+
+mkForest ::  [Version] -> [Tree Version] -> ( [Tree Version], [Version])
+mkForest  [] akku               = (akku,[])
+mkForest  (head:tail) akku      =
+        let (tree,rest) = mkTree head tail 
+        in mkForest rest (akku ++ [tree]) 
