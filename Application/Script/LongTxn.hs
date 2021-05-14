207742c62d8da0622866a74f3c62e8efe4d3ed7c
michael.fliegner <michael.fliegner@hamburg.de>
2021-05-14 19:20:41 +0200

    LongTxn entry of commit function

--- Application/Script/LongTxn.hs
+++ Application/Script/LongTxn.hs
@@ -5,9 +5,11 @@ module Application.Script.LongTxn where
 {-# LANGUAGE OverloadedStrings #-}
 import GHC.Exts
 import GHC.Generics
+import Data.Maybe
 import Generated.Types
 import Application.Script.Prelude
-import Application.Helper.Controller
+import Application.Helper.Controller 
+import IHP.Log as Log
 import Data.Aeson.Types
 import Data.ByteString.Lazy.Internal
 
@@ -29,51 +31,55 @@ instance FromJSON PersistenceLog
 
 instance ToJSON (Id' "workflows") where
     toJSON (Id key) = object ["workflowId" .= key]
-
 instance FromJSON (Id' "workflows") where
     parseJSON = withObject "workflowId" $ \o ->
         Id <$> o .: "workflowId"
-
 instance ToJSON (Id' "histories") where
     toJSON (Id key) = object ["historyId" .= key]
 instance FromJSON (Id' "histories") where
     parseJSON = withObject "historyId" $ \o ->
         Id <$> o .: "historyId"
-
 instance ToJSON (Id' "versions") where
     toJSON (Id key) = object ["versionId" .= key]
 instance FromJSON (Id' "versions") where
     parseJSON = withObject "versionId" $ \o ->
         Id <$> o .: "versionId"
-
 instance ToJSON (Id' "contracts") where
     toJSON (Id key) = object ["contractId" .= key]
 instance FromJSON (Id' "contracts") where
     parseJSON = withObject "contractId" $ \o ->
         Id <$> o .: "contractId"
-
 instance ToJSON (Id' "partners") where
     toJSON (Id key) = object ["partnerId" .= key]
 instance FromJSON (Id' "partners") where
     parseJSON = withObject "partnerId" $ \o ->
         Id <$> o .: "partnerId"
-
 instance ToJSON (Id' "tariffs") where
     toJSON (Id key) = object ["tariffId" .= key]
 instance FromJSON (Id' "tariffs") where
     parseJSON = withObject "tariffId" $ \o ->
         Id <$> o .: "tariffId"
 
-class (KnownSymbol (GetTableName rec), rec ~ GetModelByTableName (GetTableName rec), Record rec, CanCreate rec,Fetchable (QueryBuilder (GetTableName rec))  rec, FromRow rec,
-    HasField "id" rec (Id rec), Show (PrimaryKey (GetTableName rec)) , ToJSON (PrimaryKey (GetTableName rec))) => HasTxnLog rec 
+class (KnownSymbol (GetTableName rec), rec ~ GetModelByTableName (GetTableName rec), Record rec, FilterPrimaryKey (GetTableName rec),CanCreate rec,Fetchable (QueryBuilder (GetTableName rec))  rec, FromRow rec,
+    HasField "id" rec (Id rec), Show rec, Show (PrimaryKey (GetTableName rec)) , ToJSON (PrimaryKey (GetTableName rec))) => HasTxnLog rec 
     where
     mkInsertLog :: rec -> CRULog (Id rec)
     mkInsertLog row = Inserted $ get #id row
     mkUpdateLog :: rec -> rec -> CRULog (Id rec)
     mkUpdateLog old new = Updated (get #id old) (get #id new)
+    commit ::  (?modelContext::ModelContext, ?context::context, LoggingProvider context )  => CRULog (Id rec) -> IO()
+    commit cruLog = do
+        case cruLog of
+            Inserted key -> do
+                rec <- fetch key
+                putStrLn $ "huhu INSERTED " ++ show rec
+            Updated old new -> do
+                recOld <- fetch old
+                putStrLn $ "huhu OLD " ++ show recOld
+                recNew <- fetch old
+                putStrLn $ "huhu OLD " ++ show recNew
 
-
-instance HasTxnLog Workflow where
+instance HasTxnLog Workflow
 instance HasTxnLog History 
 instance HasTxnLog Version 
 instance HasTxnLog Contract
@@ -103,15 +109,15 @@ run = do
         tlogP :: CRULog (Id Partner) = mkInsertLog p
         tlogPJ = toJSON tlogP
         tlogT :: CRULog (Id Tariff) = mkInsertLog t
-        tlogCT = toJSON tlogT
+        tlogTJ = toJSON tlogT
         persistenceLog :: [PersistenceLog] =[ TariffPL tlogT, PartnerPL tlogP, ContractPL tlogC, VersionPL tlogV, HistoryPL tlogH, WorkflowPL tlogW ]
         persistenceLogJ = toJSON persistenceLog
     putStrLn $ show  tlogWJ
     putStrLn $ show  tlogHJ
     putStrLn $ show  tlogVJ
     putStrLn $ show  tlogCJ
-    putStrLn $ show  tlogP
-    putStrLn $ show  tlogCT
+    putStrLn $ show  tlogPJ
+    putStrLn $ show  tlogTJ
     v2 :: Version <- fetch $ key tlogV
     putStrLn $ show v2
     putStrLn "============================================="
@@ -121,9 +127,22 @@ run = do
     putStrLn "============================================="
     wf <- wf |> set #progress persistenceLogJ |> updateRecord
     wf :: Workflow <- fetch $ get #id wf 
-    let plog  = getPLOG wf
-    
+    let plogMB  = getPLOG wf
+        plog = fromJust plogMB
+    putStrLn "************************************************"
+    putStrLn $ "SHOWING plog of " ++ show (get #id wf) ++ show plog
+    putStrLn "************************************************"
+    putStrLn "VOR  LOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOG"
 
-    putStrLn "************************************************"
-    putStrLn $ show plog
-    putStrLn "************************************************"
+    forEach plog \pl -> do
+        putStrLn $ "Logged plog:" ++ show pl
+        case pl of
+            WorkflowPL cru -> commit cru
+            VersionPL cru -> commit cru
+            ContractPL cru -> commit cru
+            PartnerPL cru -> commit cru
+            TariffPL cru -> commit cru
+        putStrLn "Logged nach Commit"
+
+    putStrLn "NACH LOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOGLOG"
+
